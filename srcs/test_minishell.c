37c4d0becc997de5cb4a8673f0a673917d9d84f1
thallard <thallard@student.42lyon.fr>
2021-01-04 16:52:55 +0100

    Modified echo

--- srcs/test_minishell.c
+++ srcs/test_minishell.c
@@ -31,53 +31,78 @@ int	init_shell(t_shell *shell)
 	return (SUCCESS);
 }
 
+int main()
+{
+	int 	size;
+	char 	*buf;
+	char	*newenviron[] = { NULL };
+	pid_t	program;
+	t_shell	*shell;
+	t_tree	*node;
+	int		res;
+	char	**tab;
+
+	shell = malloc(sizeof(t_shell));
+	shell->ptrs = NULL;
+	// init_shell(shell);
+	buf = calloc(1, 1000);
+	if (!buf)
+		return (FAILURE);
+	size = 1;
+	while ((size = read(1, buf, 1000) > 0))
+	{
+		ft_remove_eol(buf);
+		init_shell(shell);
+		res = create_main_tree(shell, buf);
+		ft_printf("res = %d\n\n", res);
+		if (res >= 0)
+			ft_print_tree(shell->root, 0);
+		if (res == -2)
+			ft_printf("syntax error near unexpected token `%s'", shell->sep);
+		dprintf(1, "debug buf :%s\n", buf);
+		if (ft_strncmp(buf, "cd", 2) == 0)
+		{
+			node = tree_create_node(shell, "cd");
+			node->left = tree_create_node(shell, "libft");
+			ft_cd(shell, node);
+		}
+		if (ft_strncmp(buf, "pwd", 3) == 0)
+			ft_pwd(shell);
+		if (ft_strncmp(buf, "echo", 4) == 0)
+		{
+			node = tree_create_node(shell, "echo");
+			node->left = tree_create_node(shell, buf + 4);
+			dprintf(1, "je rentre devant echo\n");
+			ft_echo(shell, node);
+		}
+		ft_bzero(buf, 1000);
+			
+		// if (program == fork()) // A verifier plus tard si c'est viable
+			// execve(ft_remove_eol(tab[0]), tab, newenviron);
+		// printf("stdin --> %s\n", buf);
+	}
+	// go_to_upper_folder();
+}
+
 // int main()
 // {
 // 	int 	size;
 // 	char 	*buf;
-// 	char	*newenviron[] = { NULL };
-// 	pid_t	program;
-// 	t_shell	*shell;
-// 	int		res;
 // 	char	**tab;
+// 	char	*newenviron[] = { NULL };
+// 	int		errno;
+// 	pid_t	program;
 
-// 	shell = malloc(sizeof(t_shell));
-// 	shell->ptrs = NULL;
-// 	// init_shell(shell);
 // 	buf = calloc(1, 1000);
 // 	if (!buf)
 // 		return (FAILURE);
 // 	size = 1;
 // 	while ((size = read(1, buf, 1000) > 0))
 // 	{
-// 		ft_remove_eol(buf); // 
-// 		init_shell(shell);
-// 		res = create_main_tree(shell, buf);
-// 		ft_printf("res = %d\n\n", res);
-// 		ft_print_tree(shell->root, 0);
-// 		// if (program == fork()) // A verifier plus tard si c'est viable
-// 		// 	execve(ft_remove_eol(tab[0]), tab, newenviron);
+// 		tab = ft_split(buf, ' ');
+// 		if (program == fork()) // A verifier plus tard si c'est viable
+// 			execve(ft_remove_eol(tab[0]), tab, newenviron);
 // 		// printf("stdin --> %s\n", buf);
 // 	}
 // 	// go_to_upper_folder();
 // }
-
-int main()
-{
-	int 	size;
-	char 	*buf;
-	char	**tab;
-	t_tree	*node;
-	t_shell *shell;
-
-	buf = ft_calloc(1, 1000);
-	if (!buf)
-		return (FAILURE);
-	size = 1;
-	while ((size = read(1, buf, 1000) > 0))
-	{
-		tab = ft_split(buf, ' ');
-		if (ft_strncmp(ft_remove_eol(tab[0]), "cd", 2) == 0)
-			ft_cd(tab, node, shell);
-	}
-}
