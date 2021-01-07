7946255003fcaeeb4824fd845e853af442e61ddd
Brice Jacob <bjacob@student.42lyon.fr>
2021-01-07 09:31:43 +0100

    cd - ajout path absolu et gestion mallocs

--- srcs/commands/ft_cd.c
+++ srcs/commands/ft_cd.c
@@ -12,59 +12,86 @@
 
 #include "../../includes/minishell.h"
 
-int	go_to_upper_folder(void)
+static int	get_correct_return(int res)
+{
+	if (res == -1)
+		return (-1);
+	return (SUCCESS);
+}
+
+static char	*build_path(char *current_path, char *folder)
+{
+char	*path_temp;
+char	*path;
+
+	if (!(path = ft_strjoin(current_path, "/")))
+		return (NULL);
+	path_temp = path;
+	path = ft_strjoin(path, folder);
+	free(path_temp);
+	if (!path)
+		return (NULL);
+	path_temp = path;
+	path = ft_strtrim(path, "\n");
+	free(path_temp);
+	return (path);
+}
+
+static int	go_to_upper_folder(void)
 {
 	char	*current_path;
+	char	*path_temp;
 	char	*upper_path;
 	int		indice;
+	int		res;
 
-	current_path = ft_calloc(1, 500);
+	if (!(current_path = ft_calloc(1, 500)))
+		return (-1);
 	upper_path = NULL;
-
+	path_temp = current_path;
 	getcwd(current_path, 1000);
 	if (!current_path)
-	{
-		// ft_printf("current_path NULL\n");
-		return (-1);
-	}
-	// ft_printf("\ncurrent_path = %s\n", current_path);
-
+		return (ft_free_ptr(path_temp));
 	indice = ft_strrfind(current_path, '/');
 	if (indice > 0)
 		upper_path = ft_substr(current_path, 0, indice);
 	else
 		upper_path = current_path;
-	chdir(upper_path);
+	res = chdir(upper_path);
 	ft_free_ptr(current_path);
-	current_path = upper_path;
-	getcwd(current_path, 1000);
-	// ft_printf("current_path = %s\n", current_path);
-
+	ft_free_ptr(upper_path);
+	if (res == -1)
+		return (-1);
 	return (SUCCESS);
 }
 
-int	go_to_folder(char *folder)
+static int	go_to_folder(char *folder)
 {
 	char	*current_path;
+	char	*path_temp;
 	char	*path;
 	int		res;
 
-	current_path = ft_calloc(1, 500);
-	getcwd(current_path, 1000);
-	if (!current_path)
+	current_path = NULL;
+	if (folder[0] == '/')
 	{
-		// ft_printf("current_path NULL\n");
-		return (-1);
+		if (!(path = ft_strdup(folder))) // possible que folder == NULL ??
+			return (-1);
 	}
-	// ft_printf("\ncurrent_path = %s\n", current_path);
-	path = ft_strjoin(current_path, "/");
-	path = ft_strjoin(path, folder);
-	path = ft_strtrim(path, "\n");
+	else
+	{
+		if (!(current_path = ft_calloc(1, 500)))
+			return (-1);
+		path_temp = current_path;
+		if (!(getcwd(current_path, 1000)))
+			return (ft_free_ptr(path_temp));
+		else if (!(path = build_path(current_path, folder)))
+			return (-1);
+	}
 	res = chdir(path);
-	getcwd(current_path, 1000);
-	// ft_printf("current_path = %s\n", current_path);
-
-	return (res);
+	ft_free_ptr(current_path);
+	ft_free_ptr(path);
+	return (get_correct_return(res));
 }
 
 int		ft_cd(t_shell *shell, t_tree *node)
